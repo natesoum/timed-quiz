4b8be81340d5dabcfe8ecfe16daeff7b87d17745
natesoum <soumnathun@gmail.com>
2022-07-26 17:08:25 -0400

    Declared questions & created questions for game

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # timed-quiz
+
+## Mock Up
+
+## Objective
+
+I am creating a timed quiz website for users who would like to test their knowledge! 
+
+USER will click start game
+
+GAME will start
+- Initial screen will be hidden and first question will pop up & time will start ticking down 1 second at a time
+- USER will answer question to the best of their ability choosing from a multiple choice of 4 answers
+- USER will continue to keep answering questions until he has answered all questions 
+- ONCE user has completed answering all the questions
+- USERS score will be the time left remaining 
+- USER will save their score with their initial
+- ONCE user has submitted their initials, SCORE will be saved into local storage
+
+REFRESHING the page
+- Since we are saving the user's score in local storage, the user will be able to see their score by clicking on the "View High Scores" on the top left of the webpage
+- USERS will see previous scores ranked in highest to lowest
+- USERS will have the option to go back and play again if they would like
+
+## Description
+
+## Contributors 
+
+## Deployed Page
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,36 @@ var QuestionIndex = 0;
 
 // Gameover Variable
 var gameover;
+
+// Array of Questions
+
+var questions = [
+    { q: 'What does HTML stand for?', 
+      a: '1. HyperText Markup Language', 
+      choices: [{choice: '1. HyperText Markup Language'}, {choice: '2. HyperText Markdown Language'}, {choice: '3. Highly Technical Material Language'}, {choice: '4. Helpful Text Markup Language'}]
+    },
+    { q: 'What does CSS stand for?', 
+      a: '2. Cascading Style Sheets', 
+      choices: [{choice: '1. Cooper Styling Sheet'}, {choice: '2. Cascading Style Sheets'}, {choice: '3. Cats Super Style'}, {choice: '4. Cascade Styling Styles'}]
+    },
+    { q: 'What language do developers use for webpage functionality?', 
+      a: '3. JavaScript', 
+      choices: [{choice: '1. HTML'}, {choice: '2. CSS'}, {choice: '3. JavaScript'}, {choice: '4. Internet Explorer'}]
+    },
+    { q: 'What syntax would declare a variable named potato?', 
+      a: '4. var potato', 
+      choices: [{choice: '1. declare potato'}, {choice: '2. potato'}, {choice: '3. potato is'}, {choice: '4. var potato'}]
+    },
+    { q: 'When did JavaScript first appear?', 
+      a: '1. 1995', 
+      choices: [{choice: '1. 1995'}, {choice: '2. 1998'}, {choice: '3. 2001'}, {choice: '4. 2000'}]
+    },
+    { q: 'What does DOM stand for?', 
+      a: '1. Document Object Model', 
+      choices: [{choice: '1.  Document Object Model'}, {choice: '2. Dannys Overall Method'}, {choice: '3. Do Over Method'}, {choice: '4. Document Ordinary Model'}]
+    },
+    { q: 'What element tag allows you to connect your css file?', 
+      a: '3. link', 
+      choices: [{choice: '1. src'}, {choice: '2. href'}, {choice: '3. link'}, {choice: '4. head'}]
+    },
+  ];

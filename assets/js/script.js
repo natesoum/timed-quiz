ca7fe094f53293d0ab4c6dacc9393030521143f6
natesoum <soumnathun@gmail.com>
2022-07-26 14:53:47 -0400

    Declared container variables

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var startContainerEl = document.getElementById("start-container");
+var endContainerEl = document.getElementById("game-end-container")
+var questionContainerEl = document.getElementById("questions-container");
+var scoreContainerEl = document.getElementById("score-banner");
+var initials = document.getElementById("initial-submit");
+var correctEl = document.getElementById("correct");
+var wrongEl = document.getElementById("wrong");
+var highscoreContainerEl = document.getElementById("highscore-container");
+var viewHighScoreEl = document.getElementById("view-highscore");
+var listHighScoreEl = document.getElementById("highscore-list");
+
